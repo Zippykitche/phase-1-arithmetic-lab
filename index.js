f5b60265d0722e91f1b1cc1448dd97100d56b7ed
zippykitche <zippyk80@gmail.com>
2024-10-01 11:08:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log (num1 * num2)
+
+let random = Math.floor(Math.random()*10) +1;
+console.log(random)
+
+let num3 = 5;
+let num4 = 9;
+let mod = num4 % num3;
+console.log(mod)
+
+let max = Math.max(11,12,13,14,15,16,17,18,19,20);
+console.log(max)
